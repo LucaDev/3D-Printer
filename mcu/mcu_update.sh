ae01cfab86c63aa02e76d28e3a2b4d7b2309623a
3D Printer@LucaDev <l.kroeger01@gmail.com>
2023-04-17 13:39:24 +0100

    Autocommit from 2023-04-17 13:39:24
    Klipper on commit: cba119d
    
    Moonraker on commit: eff0901
    
    Mainsail version: v2.5.1

--- mcu/mcu_update.sh
+++ mcu/mcu_update.sh
@@ -2,12 +2,12 @@ klipper_folder=~/klipper
 
 #################
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 cd $klipper_folder
 
-make clean KCONFIG_CONFIG=config.s1
-make menuconfig KCONFIG_CONFIG=config.s1
-make KCONFIG_CONFIG=config.s1
+make clean KCONFIG_CONFIG=$SCRIPT_DIR/config.rpi
+make flash KCONFIG_CONFIG=$SCRIPT_DIR/config.rpi
 
-make clean KCONFIG_CONFIG=config.rpi
-make menuconfig KCONFIG_CONFIG=config.rpi
-make flash KCONFIG_CONFIG=config.rpi
+make clean KCONFIG_CONFIG=config.s1
+make KCONFIG_CONFIG=config.s1
