831bc74ae37cc78323b4f9b3ab729ba4b24c2b20
3D Printer@LucaDev <l.kroeger01@gmail.com>
2023-04-24 14:19:10 +0100

    Autocommit from 2023-04-24 14:19:10
    Klipper on commit: 011b4e3
    
    Moonraker on commit: 6c7dfe5
    
    Mainsail version: v2.5.1

---
+++ macros/backup/msb.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+config_namespaces=(
+"webcams"
+"timelapse"
+)
+
+getConfig() {
+  curl "http://localhost/server/database/item?namespace=$1" -s | jq -c .result.value | sed 's/\\n/\n/g'  
+}
+
+
+value=$(getConfig "mainsail")
+
+for ns in "${config_namespaces[@]}"
+do
+  value=`echo $value | jq -c --arg ns "$ns" --argjson nsConf "$(getConfig $ns)" '. + {($ns): $nsConf}'`
+done
+
+echo $value > ../../mainsail_config.json
