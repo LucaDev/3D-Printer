e4ce633a9465a94b8453447ffc0c7dde6963dade
3D Printer@LucaDev <l.kroeger01@gmail.com>
2025-10-07 20:32:53 +0100

    Autocommit from 2025-10-07 20:32:53
    Klipper on commit: 1933e01
    
    Moonraker on commit: f2bb93f
    
    Mainsail version: v2.14.0

--- mcu/mcu_update.sh
+++ mcu/mcu_update.sh
@@ -9,5 +9,7 @@ cd $klipper_folder
 make clean KCONFIG_CONFIG=$SCRIPT_DIR/config.rpi
 make flash KCONFIG_CONFIG=$SCRIPT_DIR/config.rpi
 
-make clean KCONFIG_CONFIG=config.s1
-make KCONFIG_CONFIG=config.s1
+make clean KCONFIG_CONFIG=$SCRIPT_DIR/config.skr
+make KCONFIG_CONFIG=$SCRIPT_DIR/config.skr
+
+/home/pi/katapult/scripts/flashtool.py -d /dev/serial/by-id/usb-katapult_lpc1768_23800012658C48AFAE15345DC52000F5-if00 -f /home/pi/klipper/out/klipper.bin
